fe3da7db39917b3ff031bc3b479efb8eb79b538c
tunisia0011 <82506259+tunisia0011@users.noreply.github.com>
2021-09-23 04:01:36 +0800

    update

--- checkin.py
+++ checkin.py
@@ -22,24 +22,19 @@ LoginData = {
 }
 rLogin = session.post(url = LoginUrl, data = LoginData, headers = LoginHeaders)
 
-if rLogin.headers['content-length'] == '42' :
-    print('机场登录成功')
-    CheckUrl = Url + '/user/checkin'
-    CheckHeaders = {
+CheckUrl = Url + '/user/checkin'
+CheckHeaders = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 Edg/93.0.961.52'
     }
-    rCheck = session.post(url = CheckUrl, headers = CheckHeaders)
-    str = rCheck.content.decode('utf-8').split("\"")[5].encode('utf8').decode('unicode_escape')
-    print(str)
-    TelegramPush = 'https://api.telegram.org/bot' + BOTTOKEN + '/sendMessage?chat_id=' + TGCHATID + '&text=' + str
-    rPush = requests.get(TelegramPush)
-    if rPush.status_code == 200 :
-        print('推送成功')
-    elif rPush.status_code == 400 :
-        print('CHATID 填写有误')
-    else :
-        print('推送失败，未知错误')
-else:
-    print('机场登陆失败')
-    sys.exit(1)
+rCheck = session.post(url = CheckUrl, headers = CheckHeaders)
+str = rCheck.content.decode('utf-8').split("\"")[5].encode('utf8').decode('unicode_escape')
+print(str)
+TelegramPush = 'https://api.telegram.org/bot' + BOTTOKEN + '/sendMessage?chat_id=' + TGCHATID + '&text=' + str
+rPush = requests.get(TelegramPush)
+if rPush.status_code == 200 :
+    print('推送成功')
+elif rPush.status_code == 400 :
+    print('CHATID 填写有误')
+else :
+    print('推送失败，未知错误')
